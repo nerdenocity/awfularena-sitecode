7a5813b80be331a6c025edde65649f2b69880e2e
aba <nerdenocity@gmail.com>
2025-05-29 01:23:33 -0500

    fixed connect link due to steam-connect-link having issues with consistent DNS stuff

--- tf2/default.php
+++ tf2/default.php
@@ -1,3 +1,3 @@
 <!DOCTYPE html>
-<meta http-equiv="refresh" content="0; url=steam://connect/server.awfularena.com:27015" /> 
+<meta http-equiv="refresh" content="0; url=steam://connect/82.25.86.80:27015" /> 
 <html lang="en"></html>
